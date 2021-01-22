747ed1e15f106c9ba986ebbe438e25b0d7f60936
Joshua Burger <joshberger6969@outlook.com>
2021-01-22 10:34:18 -0500

    commit tree eecc34a70153248ec49b16d7e27196dd45e02d41

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is josh"
+      end
+
+    get '/hometown' do
+        "My hometown is Weatherly"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is nada"
+    end 
 end
